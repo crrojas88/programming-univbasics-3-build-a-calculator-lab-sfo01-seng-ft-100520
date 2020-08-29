6727e5a2de706444e8b1d1b424306137f3efe9eb
Rodrigo Rojas <carlorrojas@gmail.com>
2020-08-29 22:28:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
 def addition(num1, num2)
-  num2 + num1
+  result = num2 + num1
+  return result
 end
 
 def subtraction(num1, num2)
-
+  result = num1 - num2
+  return result
 end
 
 def division(num1, num2)
-
+  result = num1 / num2
+  return result
 end
 
 def multiplication(num1, num2)
-
+  result = num1 * num2
+  return result
 end
 
 def modulo(num1, num2)
-
+  result = num1 % num2
+  return result
 end
 
 def square_root(num)
-
+  result = num ^ 2
 end
